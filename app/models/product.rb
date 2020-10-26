321b37c219bd435d4db879d9b2468df18901a393
daniel <shiriux1@gmail.com>
2020-10-25 22:06:02 -0500

    adding line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,18 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   # add database level index and constraints to this validations
   validates :description, :title, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\z/i, message: 'Must be a url for gif, jpg, png image' }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
